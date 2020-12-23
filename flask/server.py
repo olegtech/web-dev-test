5afb96f9eb0c116f2dfd1e3ed8e9515d859e3c7a
olegtech <oleg.sarbaev@gmail.com>
2020-12-23 13:16:37 -0800

    flask edit contact form

--- flask/server.py
+++ flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -20,4 +20,14 @@ def home():
 
 @app.route('/<string:page_name>')
 def page(page_name):
-    return render_template(page_name)        
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		data = request.form.to_dict()
+		print(data)
+		return redirect('/thanks.html')
+	else:
+		return "something went wrong("	
