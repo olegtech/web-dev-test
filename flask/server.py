252b5b08e2e3fa4744f0ad565c28f1907c717b21
olegtech <oleg.sarbaev@gmail.com>
2020-12-23 11:58:51 -0800

    add html template

--- flask/server.py
+++ flask/server.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+print(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Oleg!'
+def home():
+    return render_template('index.html')
 
-@app.route('/blog')
-def blog():
-    return 'test blog page!'
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')    
+# 
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+# 
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+@app.route('/<string:page_name>')
+def page(page_name):
+    return render_template(page_name)        
